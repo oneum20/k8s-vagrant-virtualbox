86aecf4078fc6320b4ecacc42dcffb2237eb2ab5
han2290 <woom2290@gmail.com>
2022-04-26 14:01:11 +0900

    Add bootstrap.sh

--- k8s/bootstrap-node.sh
+++ k8s/bootstrap-node.sh
@@ -13,13 +13,14 @@ sudo apt-get install -y \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
+# TODO: Docker Version
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-# Set Cgroup Driver
+# Set CgroupDriver
 cat <<EOF > /etc/docker/daemon.json
 {
   "exec-opts": ["native.cgroupdriver=systemd"]
@@ -33,28 +34,16 @@ sudo systemctl restart docker
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubelet=${KUBE_VERSION} kubeadm=${KUBE_VERSION} kubectl=${KUBE_VERSION}
-sudo apt-mark hold kubelet kubeadm kubectl
+sudo apt-get install -y kubelet=${KUBE_VERSION} kubeadm=${KUBE_VERSION} 
+sudo apt-mark hold kubelet kubeadm
 
-# Node IP
+# Set node ip
 # sudo sed -i 's|KUBELET_KUBEADM_ARGS="|KUBELET_KUBEADM_ARGS="--node-ip='${IP_ADDR}' |' /var/lib/kubelet/kubeadm-flags.env
 echo "KUBELET_EXTRA_ARGS=\"--node-ip=${IP_ADDR}\"" > /etc/default/kubelet
-# swap off
+
+# Swap off
 sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-# kubeadm init
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=${IP_ADDR}
-
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-export KUBECONFIG=/etc/kubernetes/admin.conf
-
-# install CNI
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-
-# join cmd
-rm -f /vagrant/join-command && \
-kubeadm token create --print-join-command > /vagrant/join-command
+# Join
+bash -c "$(cat /vagrant/join-command) -v=5" > /vagrant/logs
